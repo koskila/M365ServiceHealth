738cd7e97e5fd9b5f0e73894d79ef22a6a447932
Antti K. Koskela <posti@koskila.net>
2023-03-13 11:45:37 +0200

    Add environment variables

--- ServiceHealthReader/ServiceHealthReader.Web/Program.cs
+++ ServiceHealthReader/ServiceHealthReader.Web/Program.cs
@@ -7,18 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 //builder.Configuration.AddJsonFile("appsettings.json", false);
 builder.Configuration.AddJsonFile("appsettings.local.json", true);
-
-// Add services to the container.
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add services to the container.
 builder.Services.AddScoped<IServiceConfiguration, ServiceConfiguration>();
 builder.Services.AddScoped<IDbService, DbService>();
 
-// instantiate a new applicationdbcontext with a connectionstring from appsettings
+// instantiate a new applicationdbcontext with a connectionstring from appsettings (or environment variables)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
